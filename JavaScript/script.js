33b87829958e32a611eedeaa05eba67cc8aa37b2
Gerges Samy <jirgissamy78@gmail.com>
2024-07-09 11:10:49 +0300

    render task in section tasks

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 class Task {
   #date = new Date();
-  #id = (Date.now() + "").slice(-10) + Math.floor(Math.random() * 5) + 1;
+  #id = (Date.now() + '').slice(-10) + Math.floor(Math.random() * 5) + 1;
   #taskContent;
-  #taskState = "incomplete";
+  #taskState = 'incomplete';
   constructor() {}
 
   get getDate() {
@@ -15,7 +15,7 @@ class Task {
   get getTaskContent() {
     return this.#taskContent;
   }
-  set _setTaskContent(content = "") {
+  set _setTaskContent(content = '') {
     this.#taskContent = content;
   }
 
@@ -30,36 +30,36 @@ class Task {
 class IncompleteTask extends Task {
   constructor() {
     super();
-    this._setTaskStateCompleteOrIncomplete = "incomplete";
+    this._setTaskStateCompleteOrIncomplete = 'incomplete';
   }
 }
 
 class CompleteTask extends Task {
   constructor() {
     super();
-    this._setTaskStateCompleteOrIncomplete = "complete";
+    this._setTaskStateCompleteOrIncomplete = 'complete';
   }
 }
 
 /////////////////////////////////
-const container = document.querySelector(".container");
-const toDoApp = document.querySelector(".todoApp");
-const toDoAppHeader = document.querySelector(".todoApp__header");
-const toDoAppDate = document.querySelector(".todoApp-date");
-const toDoAppDay = document.querySelector(".todoApp-day--number");
+const container = document.querySelector('.container');
+const toDoApp = document.querySelector('.todoApp');
+const toDoAppHeader = document.querySelector('.todoApp__header');
+const toDoAppDate = document.querySelector('.todoApp-date');
+const toDoAppDay = document.querySelector('.todoApp-day--number');
 const toDoAppMonthAndYearContainer = document.querySelector(
-  ".todoApp-month-year"
+  '.todoApp-month-year'
 );
-const month = document.querySelector(".month");
-const year = document.querySelector(".year");
-const toDoAppDayString = document.querySelector(".todoApp-day--string");
-const toDoAppBody = document.querySelector(".todoApp__body");
-const taskGroup = document.querySelector(".task__list");
-const task = document.querySelector(".task");
-const btnCreate = document.querySelector(".btn__create");
-const btnSave = document.querySelector(".btn__save");
-const form = document.querySelector(".form");
-const inputField = document.querySelector(".input-field");
+const month = document.querySelector('.month');
+const year = document.querySelector('.year');
+const toDoAppDayString = document.querySelector('.todoApp-day--string');
+const toDoAppBody = document.querySelector('.todoApp__body');
+const taskGroup = document.querySelector('.task__list');
+const task = document.querySelector('.task');
+const btnCreate = document.querySelector('.btn__create');
+const btnSave = document.querySelector('.btn__save');
+const form = document.querySelector('.form');
+const inputField = document.querySelector('.input-field');
 
 class ToDoApp {
   #date = new Date();
@@ -84,15 +84,15 @@ class ToDoApp {
   }
 
   _addClickEventTargetAndCallBackFunction(target, callBackFunction) {
-    target.addEventListener("click", callBackFunction);
+    target.addEventListener('click', callBackFunction);
   }
 
   _activeBtnCreate() {
-    btnCreate.classList.toggle("btn--active");
+    btnCreate.classList.toggle('btn--active');
   }
 
   _renderTaskCreationFiled() {
-    form.classList.toggle("hide");
+    form.classList.toggle('hide');
   }
 
   _makeNewTask() {
@@ -103,26 +103,61 @@ class ToDoApp {
     this._renderSelectedElementToggle(form);
     console.log(task.getTaskContent);
     this._clearInputFieldValue(inputField);
+    this._renderTask(task, 'input');
   }
 
   _isInputNull(inputFieldValue) {
-    return inputFieldValue === "" ? true : false;
+    return inputFieldValue === '' ? true : false;
   }
 
   _renderSelectedElementToggle(selector) {
-    selector.classList.toggle("hide");
+    selector.classList.toggle('hide');
   }
 
   _clearInputFieldValue(Element) {
-    Element.value = "";
+    Element.value = '';
+  }
+
+  _renderTask(task, type = 'input') {
+    const markup = this._generateTaskMarkup(task, type);
+    taskGroup.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  _generateTaskMarkup(task, type = 'input') {
+    const taskID = task.getId;
+    const taskContent = task.getTaskContent;
+    return `
+    <div class="task" id="task-${taskID}">
+
+              <li>${taskContent}</li>
+              ${this._generateTaskInputTypeMarkup(type)}
+              
+    </div>
+    `;
+  }
+
+  _generateTaskInputTypeMarkup(inputType = 'input') {
+    if (inputType === 'input')
+      return `<div class="type--input">
+                <input type="checkbox" name="task" value="1"" />
+              </div>`;
+    if (inputType === 'checked')
+      return `
+              <div class="type--checked">
+                <img src="./assets/Icons/checked.png" alt="" />
+                </div>
+                `;
   }
 
   _hideSelectedModel(selector) {
-    selector.style.display = "none";
+    selector.style.display = 'none';
   }
 
   _showSelectedModel(selector) {
-    selector.style.display = "auto";
+    selector.style.display = 'auto';
+  }
+  _clear(container) {
+    container.innerHTML = '';
   }
 }
 
